1a79c8a39c40e9229d0693ac8a6a66c5ea6da3e8
Santiago Barreda <sbarreda@ucdavis.edu>
2022-10-15 09:39:20 -0700

    Update exercise_models.R

--- data/exercise_models.R
+++ data/exercise_models.R
@@ -90,7 +90,6 @@ model_sum_coding_t_ex = brms::add_criterion(model_sum_coding_t_ex, "loo")
 
 
 ### Chapter 6
-
 options (contrasts = c('contr.sum','contr.sum'))
 notmen = exp_ex[exp_ex$C_v!='m' & exp_ex$C!='m',]
 
@@ -112,6 +111,7 @@ model_re_t_ex = brms::add_criterion(model_re_t_ex, "loo")
 
 
 ### Chapter 7
+options (contrasts = c('contr.sum','contr.sum'))
 
 priors = c(brms::set_prior("student_t(3,156, 12)", class = "Intercept"),
            brms::set_prior("student_t(3,0, 12)", class = "b"),
@@ -128,4 +128,127 @@ model_interaction_ex =
 model_interaction_ex = brms::add_criterion(model_interaction_ex, "loo")
 
 # saveRDS (model_interaction_ex, '../models/7_model_interaction_ex.RDS')
+
+
+
+### Chapter 8
+options (contrasts = c('contr.sum','contr.sum'))
+
+model_formula = brms::bf(height ~ A*G + (A*G|x|L) + (1|S),
+                         sigma ~ A + (A|x|L))
+priors = 
+  c(set_prior("student_t(3, 156, 12)", class = "Intercept"),
+    set_prior("student_t(3, 0, 12)", class = "b"),
+    set_prior("student_t(3, 0, 12)", class = "sd"),
+    set_prior("gamma(2, 0.1)", class = "nu"),
+    set_prior("normal(0, 1.5)", class = "Intercept", dpar = "sigma"),
+    set_prior("normal(0, 1.5)", class = "b", dpar = "sigma"),
+    set_prior("normal(0, 1.5)", class = "sd", dpar = "sigma"),
+    set_prior("lkj_corr_cholesky (2)", class = "cor"))
+
+model_A_L_sigma_ex = 
+  brms::brm (model_formula, data = exp_ex, chains = 4, cores = 4,
+             warmup = 1000, iter = 3500, thin = 2, family="student",
+             prior = priors)
+
+model_A_L_sigma_ex = brms::add_criterion(model_A_L_sigma_ex, "loo")
+
+# saveRDS (model_A_L_sigma_ex, '../models/8_model_A_L_sigma_ex.RDS')
+
+
+### Chapter 9
+options (contrasts = c('contr.sum','contr.sum'))
+
+priors = c(brms::set_prior("student_t(3,160, 12)", class = "Intercept"),
+           brms::set_prior("student_t(3,0, 12)", class = "b"),
+           brms::set_prior("student_t(3,0, 12)", class = "sd"),
+           brms::set_prior("lkj_corr_cholesky (2)", class = "cor"), 
+           brms::set_prior("gamma(2, 0.1)", class = "nu"),
+           brms::set_prior("student_t(3,0, 12)", class = "sigma"))
+
+model_random_slopes_complex_ex =  
+  brms::brm (height ~ vtl*A*G + (vtl*A*G|L) + (1|S), data = exp_ex, 
+             chains = 4, cores = 4, warmup = 1000, iter = 5000, thin = 4, 
+             prior = priors, family = "student", control = list(adapt_delta = 0.95))
+
+model_random_slopes_complex_ex = brms::add_criterion(model_random_slopes_complex_ex, "loo")
+
+##  saveRDS (model_random_slopes_complex_ex, '../models/9_model_random_slopes_complex_ex.RDS')
+
+
+
+
+
+### Chapter 10
+options (contrasts = c('contr.sum','contr.sum'))
+
+priors = c(brms::set_prior("student_t(3, 0, 3)", class = "Intercept"),
+           brms::set_prior("student_t(3, 0, 3)", class = "b"),
+           brms::set_prior("student_t(3, 0, 3)", class = "sd"),
+           brms::set_prior("lkj_corr_cholesky (2)", class = "cor"))
+
+model_gender_vtl_ex =
+  brm (Female ~ vtl*A + (vtl*A|L) + (1|S), data=exp_ex, chains=4, cores=4, 
+       family="bernoulli", warmup=1000, iter= 5000, thin = 4,prior=priors)
+
+model_gender_vtl_ex = brms::add_criterion(model_gender_vtl_ex, "loo")
+
+# saveRDS (model_gender_vtl, '../models/10_model_gender_vtl.RDS')
+
+
+model_gender_dt_ex =
+  brm (Female ~ F_v*A_v + (F_v*A_v|L) + (1|S), data=exp_ex, 
+       chains=4, cores=4, family="bernoulli", 
+       warmup=1000, iter = 5000, thin = 4,  
+       prior = c(set_prior("student_t(3, 0, 3)", class = "Intercept"),
+                 set_prior("student_t(3, 0, 3)", class = "b"),
+                 set_prior("student_t(3, 0, 3)", class = "sd"),
+                 set_prior("lkj_corr_cholesky (2)", class = "cor")))
+
+model_gender_dt_ex = brms::add_criterion(model_gender_dt_ex, "loo")
+
+# saveRDS (model_gender_dt_ex, '../models/10_model_gender_dt_ex.RDS')
+
+
+
+
+### Chapter 11
+options (contrasts = c('contr.sum','contr.sum'))
+
+priors = c(brms::set_prior("student_t(3,0, 12)", class = "Intercept"),
+           brms::set_prior("student_t(3,0, 12)", class = "b"),
+           brms::set_prior("student_t(3,0, 12)", class = "sd"),
+           brms::set_prior("lkj_corr_cholesky (2)", class = "cor"), 
+           brms::set_prior("gamma(2, 0.1)", class = "nu"),
+           brms::set_prior("student_t(3,0, 12)", class = "sigma"))
+
+model_height_vtl_f0_ex =  
+  brms::brm (height ~ vtl*f0*A*G + (vtl*f0*A*G|L) + (1|S), data = exp_ex, 
+             chains = 4, cores = 4, warmup = 1000, iter = 5000, thin = 4, 
+             prior = priors, family = "student")
+
+model_height_vtl_f0_ex = brms::add_criterion(model_height_vtl_f0_ex, "loo")
+
+# saveRDS (model_height_vtl_f0_ex, '../models/11_model_height_vtl_f0_ex.RDS')
+
+
+model_gender_vtl_f0_reduced_ex =
+  brm (Female ~ (vtl+f0)*A + ((vtl+f0)*A|L) + (1|S), data=exp_data, 
+       chains=4, cores=4, family="bernoulli", 
+       warmup=1000, iter = 5000, thin = 4,  
+       prior = c(set_prior("student_t(3, 0, 3)", class = "Intercept"),
+                 set_prior("student_t(3, 0, 3)", class = "b"),
+                 set_prior("student_t(3, 0, 3)", class = "sd"),
+                 set_prior("lkj_corr_cholesky (2)", class = "cor")))
+
+model_gender_vtl_f0_reduced_ex = brms::add_criterion(model_gender_vtl_f0_reduced_ex, "loo")
+
+# saveRDS (model_gender_vtl_f0_reduced_ex, '../models/11_model_gender_vtl_f0_reduced_ex.RDS')
+
+
+
+
+
+### Chapter 12
+options (contrasts = c('contr.sum','contr.sum'))
 
